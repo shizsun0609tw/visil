f00343e0a9639c1a1e2460b7e45f67fc470f5d47
shizsun0609tw <shizsun0609tw@gmail.com>
2020-12-07 10:29:31 +0800

    update output sort

--- outputprocessing.py
+++ outputprocessing.py
@@ -8,14 +8,15 @@ in_file = open(in_path, 'r')
 out_file = open(out_path, 'w')
 
 data = json.load(in_file)
-out_data = dict()
+out_data = []
 
 for video in data.keys():
     video_similarity = sorted(data[video].items(), key = lambda x:x[1], reverse = True)
-    video_similarity = list((x, y) for x, y in video_similarity if y > 0 and x != video)
+    video_similarity = list((x, y) for x, y in video_similarity)[:3]
 
-    out_data.update({video:[video_similarity]})
+    out_data.append([video, video_similarity])
 
+out_data.sort(key = lambda x:(x[1][0][1], x[1][1][1], x[1][2][1]), reverse=True)
 json.dump(out_data, out_file, indent = 1)
 
 in_file.close()
